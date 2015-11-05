0cb486659d4ec0adf9330e10a30f3b48c24f5646
Durant Schoon <durant.schoon@gmail.com>
2015-11-04 19:50:28 -0800

    remove daniel's configuration to turn off things

--- init.el
+++ init.el
@@ -1,13 +1,4 @@
 ;;; Begin initialization
-;; Turn off mouse interface early in startup to avoid momentary display
-(when window-system
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
-
-(setq inhibit-startup-message t)
-(setq initial-scratch-message "")
 
 ;;; Set up package
 (require 'package)
