8243c479ed97d31351e3a6fc32f468741f7b745d
Durant Schoon <durant.schoon@gmail.com>
2019-03-21 15:47:18 -0700

    add indium for Javascript debugging

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default)))
  '(fci-rule-color "#383838")
+ '(go-guru-scope "...")
  '(grep-find-ignored-directories
    (quote
     ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "log" "tmp")))
@@ -19,7 +20,7 @@
  '(line-number-mode nil)
  '(package-selected-packages
    (quote
-    (projectile-speedbar helm-etags-plus yasnippet-snippets json-mode anaconda-mode emojify emojify-mode buffer-move go-guru helm-company gore-mode company-go company company-mode autopair go-mode solidity-mode pcre2el color-theme-sanityinc-tomorrow color-theme helm-swoop helm-ag ag helm-projectile helm visual-regexp-steroids visual-regexp url-shortener phi-search-mc rainbow-mode osx-plist org-jira clojure-mode octave-mode yasnippet yaml-mode web-mode use-package smartscan sass-mode python-mode projectile pp-c-l phi-search org-bullets multiple-cursors monokai-theme markdown-mode magit livescript-mode js2-mode ido-vertical-mode flycheck exec-path-from-shell emmet-mode edit-server cyberpunk-theme csharp-mode coffee-mode cl-lib-highlight)))
+    (indium indium-mode projectile-speedbar helm-etags-plus yasnippet-snippets json-mode anaconda-mode emojify emojify-mode buffer-move go-guru helm-company gore-mode company-go company company-mode autopair go-mode solidity-mode pcre2el color-theme-sanityinc-tomorrow color-theme helm-swoop helm-ag ag helm-projectile helm visual-regexp-steroids visual-regexp url-shortener phi-search-mc rainbow-mode osx-plist org-jira clojure-mode octave-mode yasnippet yaml-mode web-mode use-package smartscan sass-mode python-mode projectile pp-c-l phi-search org-bullets multiple-cursors monokai-theme markdown-mode magit livescript-mode js2-mode ido-vertical-mode flycheck exec-path-from-shell emmet-mode edit-server cyberpunk-theme csharp-mode coffee-mode cl-lib-highlight)))
  '(safe-local-eval-forms
    (quote
     ((add-hook
@@ -49,6 +50,10 @@
  '(mode-line ((t (:background "blue4" :foreground "gray90"))))
  '(mode-line-buffer-id ((t (:foreground "gold1" :weight ultra-bold))))
  '(mode-line-inactive ((t (:background "#404045" :foreground "gray60"))))
+ '(org-block ((t (:background "#000000"))))
+ '(org-block-background ((t (:background "#000000"))))
+ '(org-block-begin-line ((t (:foreground "#008ED1" :background "#002E41"))))
+ '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
  '(show-paren-match ((t (:background "default" :foreground "#afa" :weight ultra-bold))))
  '(show-paren-mismatch ((t (:background "default" :foreground "#cc6666" :weight ultra-bold))))
  '(which-func ((t (:foreground "orange")))))
