5b62613288e13655b7612ba1c738e090039ea14d
Durant Schoon <durant.schoon@DSCHOONWORK.local>
2019-05-21 11:16:05 -0700

    changes I realized I needed after staring a new job

--- custom.el
+++ custom.el
@@ -20,7 +20,7 @@
  '(line-number-mode nil)
  '(package-selected-packages
    (quote
-    (doom-modeline all-the-icons company-anaconda avy use-package-chords expand-region indium indium-mode projectile-speedbar helm-etags-plus yasnippet-snippets json-mode anaconda-mode emojify emojify-mode buffer-move go-guru helm-company gore-mode company-go company company-mode autopair go-mode solidity-mode pcre2el color-theme-sanityinc-tomorrow color-theme helm-swoop helm-ag ag helm-projectile helm visual-regexp-steroids visual-regexp url-shortener phi-search-mc rainbow-mode osx-plist org-jira clojure-mode octave-mode yasnippet yaml-mode web-mode use-package smartscan sass-mode python-mode projectile pp-c-l phi-search org-bullets multiple-cursors monokai-theme markdown-mode magit livescript-mode js2-mode ido-vertical-mode flycheck exec-path-from-shell emmet-mode edit-server cyberpunk-theme csharp-mode coffee-mode cl-lib-highlight)))
+    (color-theme-modern doom-modeline all-the-icons company-anaconda avy use-package-chords expand-region indium indium-mode projectile-speedbar helm-etags-plus yasnippet-snippets json-mode anaconda-mode emojify emojify-mode buffer-move go-guru helm-company gore-mode company-go company company-mode autopair go-mode solidity-mode pcre2el color-theme-sanityinc-tomorrow color-theme helm-swoop helm-ag ag helm-projectile helm visual-regexp-steroids visual-regexp url-shortener phi-search-mc rainbow-mode osx-plist org-jira clojure-mode octave-mode yasnippet yaml-mode web-mode use-package smartscan sass-mode python-mode projectile pp-c-l phi-search org-bullets multiple-cursors monokai-theme markdown-mode magit livescript-mode js2-mode ido-vertical-mode flycheck exec-path-from-shell emmet-mode edit-server cyberpunk-theme csharp-mode coffee-mode cl-lib-highlight)))
  '(safe-local-eval-forms
    (quote
     ((add-hook
@@ -51,6 +51,10 @@
  '(mode-line ((t (:background "blue4" :foreground "gray90"))))
  '(mode-line-buffer-id ((t (:foreground "gold1" :weight ultra-bold))))
  '(mode-line-inactive ((t (:background "#404045" :foreground "gray60"))))
+ '(org-block ((t (:background "#000000"))))
+ '(org-block-background ((t (:background "#000000"))))
+ '(org-block-begin-line ((t (:foreground "#008ED1" :background "#002E41"))))
+ '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
  '(show-paren-match ((t (:background "default" :foreground "#afa" :weight ultra-bold))))
  '(show-paren-mismatch ((t (:background "default" :foreground "#cc6666" :weight ultra-bold))))
  '(which-func ((t (:foreground "orange")))))
